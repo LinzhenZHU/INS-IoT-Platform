fbb7befa85bfff73463560faba31a73cbad13d24
Linzhen (Brian) Zhu <146813475+LinzhenZHU@users.noreply.github.com>
2024-11-09 22:04:24 -0500

    Update main.m

--- main.m
+++ main.m
@@ -1,27 +1,3 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%>
-%> @file Main.m
-%>                                                                           
-%> @brief This is the skeleton file for processing data from a foot mounted
-%> IMU. The data is processed using a Kalman filter based zero-velocity 
-%> aided inertial navigation system algorithm.
-%> 
-%> @details This is the skeleton file for processing data from a foot
-%> mounted inertial measurement unit (IMU). The data is processed using a 
-%> Kalman filter based zero-velocity aided inertial navigation system. The
-%> processing is done in the following order. 
-%> 
-%> \li The IMU data and the settings controlling the Kalman filter is loaded.
-%> \li The zero-velocity detector process all the IMU data.
-%> \li The filter algorithm is processed.
-%> \li The in data and results of the processing is plotted.
-%>
-%> @authors Isaac Skog, John-Olof Nilsson
-%> @copyright Copyright (c) 2011 OpenShoe, ISC License (open source)
-%>
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 % Assuming an indefinite loop. Break condition not specified.
 % Open a new file named 'coordinates.csv' for writing
 fileID = fopen('A.csv', 'w');
